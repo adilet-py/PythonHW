3bea1dd2bac5fd1e81094520fe32345eca4152c8
Adilet-py <z.skinny.fy@gmail.com>
2022-10-01 00:36:40 +0900

    hw#8

--- Phone_Book/search_data.py
+++ Phone_Book/search_data.py
@@ -1,12 +1,17 @@
 # модуль поиска контакта
 
-from export_data import export_data
+from read_data import read_data
 from print_data import print_data
 
 def search_data(word, data):
     if len(data) > 0:
+        lst = []
         for item in data:
             if word in item:
-                return item
+                lst.append(item)
+        if lst == []:
+            return None
+        else:
+            return lst
     else:
         return None
